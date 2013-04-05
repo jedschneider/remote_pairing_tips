a48cf0eddce79a959350ed88e9a0711f9e1b2702
Jed Schneider <jed.schneider@modeset.com>
2013-04-05 11:13:48 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carrer::Application.config.secret_token = 'f600100b249599d09ce7e901866b6b00fa17a90a57ae24284b7cf8b3d2e31270d2cf1055a3174de49c906fd9e3043fc57023cdaba2a44233e38f080eef381ce6'
